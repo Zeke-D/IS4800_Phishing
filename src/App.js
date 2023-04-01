7643da7d2db28282f6ffd1a6038ca07dbde6008c
Zeke-D <zeke.dascoli@gmail.com>
2023-04-01 15:14:56 -0400

    Initializing repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Article(props) {
+
+  return (
+    <article>
+      <div class='profpic'></div>
+      <span>Posted by {props.user}</span>
+      <p>{props.body}</p>
+      <h1>{props.headline}</h1>
+    </article>
+  );
+  
+}
+
+let article = {
+  user: "user1",
+  link: "www.google.com",
+  postTime: new Date(),
+  image: "",
+  body: `This is a really neat little site I found. This is a brief description of it, 
+      and I thought you might like to check it out. In fact, here is another link 
+      to check out while you're at it:
+  `,
+  headline: "Headline",
+}
+
+let articles = [ article, article, article ]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link rel="stylesheet" href='style.css'></link>
+      <main>
+        { articles.map(article => <Article {...article}/>) }
+      </main>
     </div>
   );
 }
